4f726586bc2dc4219e1cb594dca686a8559ccf85
김형석 <165638682+Hyungs0703@users.noreply.github.com>
2024-11-18 14:22:32 +0900

    [level 0] Title: rny_string, Time: 0.32 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/0/181863. rny＿string/rny＿string.java
+++ 프로그래머스/0/181863. rny＿string/rny＿string.java
@@ -1,16 +1,5 @@
 class Solution {
     public String solution(String rny_string) {
-        StringBuilder answer = new StringBuilder(); 
-        char[] charArray = rny_string.toCharArray(); 
-
-        for (char s : charArray) {
-            if (s == 'm') { 
-                answer.append("rn");
-            } else { 
-                answer.append(s);
-            }
-        }
-
-        return answer.toString(); 
+        return rny_string.replaceAll("m","rn");
     }
 }
