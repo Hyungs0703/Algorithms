001075589b423b89e1bd1c29656d702faa43880a
김형석 <165638682+Hyungs0703@users.noreply.github.com>
2024-11-21 10:45:48 +0900

    [level 0] Title: ad 제거하기, Time: 9.92 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -2,13 +2,8 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] strArr) {
-        List<String> str = new ArrayList<>();
-                
-        for(String s : strArr) {
-            if(!s.contains("ad")) {
-                str.add(s);
-            }
-        }
-        return str.toArray(new String[0]);
+       return Arrays.stream(strArr)
+           .filter(s -> !s.contains("ad"))
+           .toArray(String[]::new);
     }
 }
